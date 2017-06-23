370dad9251aa246de4e3b21466664bfdb252b3a2
Diana Eastman <diana.eastman@annkissam.com>
2017-06-23 15:53:55 -0400

    Add helper to apply styling to flash notice based on the type of alert.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_name_to_bootstrap_alert(name)
+    case name.to_s
+    when 'notice'
+      'success'
+    when 'error'
+      'danger'
+    else
+      'info'
+    end
+  end
 end
