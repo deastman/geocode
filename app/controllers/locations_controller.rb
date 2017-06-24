47fc07bfbb707623b4445a47c0c926fc5da87f3e
Diana Eastman <diana.eastman@annkissam.com>
2017-06-24 16:12:04 -0400

    Use translations to clean up flash messages. Reorganize logic in the controller to account for incorporation of place_id.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,37 +1,36 @@
 class LocationsController < ApplicationController
+  before_filter :set_locations
+
   def new
-    set_locations
   end
 
   def create
-    msg = nil
+    if location_params[:place_id].present?
+      @location_results = Location.find_by_place_id(location_params[:place_id]).first
 
-    if query_params_present_in_form?
-      results = call_to_api_for_location_data
-      parsed_results = parse_api_results(results)
+      if @location_results.blank?
+        parsed_results = call_parse_and_return_api_results
 
-      if parsed_results[:data].empty?
-        msg = 'No results match your query. Please try another search.'
+        if parsed_results[:data].empty?
+          flash.now[:error] = t('no_results')
+        else
+          @location_results = LocationResults.new(parsed_results[:data].first)
+
+          if create_new_location(@location_results)
+            flash.now[:notice] = t('new_result_and_save_success', latitude: @location_results.latitude, longitude: @location_results.longitude)
+          else
+            flash.now[:notice] = t('new_result_and_save_failure', latitude: @location_results.latitude, longitude: @location_results.longitude)
+          end
+        end
       else
-        @location_results = LocationResults.new(parsed_results[:data].first)
-
-        if !location_already_in_db?(@location_results)
-          create_new_location(@location_results)
-        end
-
-        msg = "Your location has coordinates #{@location_results.latitude}, #{@location_results.longitude}."
-
-        if @location_results.partial_match
-          msg = msg + " " + "The geocoder was only able to match part of your search, so these results are approximate"
-        end
-
-        flash[:notice] = msg
-       end
+        flash.now[:notice] = t('location_already_exists', latitude: @location_results.latitude, longitude: @location_results.longitude)
+      end
     else
-      flash[:error] = 'Please fill in at least one form field.'
+      flash.now[:error] = t('invalid_search')
     end
 
     set_locations
+
     render :new
   end
 
@@ -41,8 +40,13 @@ class LocationsController < ApplicationController
     location_params.any? { |_, v| v.present? }
   end
 
+  def call_parse_and_return_api_results
+    results = call_to_api_for_location_data
+    parse_api_results(results)
+  end
+
   def call_to_api_for_location_data
-    GoogleMapsApiCaller.new(location_params.deep_dup).call
+    GoogleMapsApiCaller.new(params_place_id).call
   end
 
   def parse_api_results(results)
@@ -63,23 +67,21 @@ class LocationsController < ApplicationController
     location.city = location_results.city
     location.state = location_results.state_code
     location.country = location_results.country
+    location.place_id = location_results.place_id
 
     location.save!
   end
 
   def location_already_in_db?(location_results)
-    latitude = BigDecimal.new(location_results.latitude, 9)
-    longitude = BigDecimal.new(location_results.longitude, 8)
-
-    Location.find_by_latitude_longitude(latitude, longitude).any?
+    Location.find_by_place_id(location_results.place_id).any?
   end
 
-  def retrieve_location_from_db_by_form_input
-    Location.find_by_form_attributes(location_params)
+  def params_place_id
+    location_params[:place_id]
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def location_params
-    params.require(:location).permit(:street_address, :city, :state, :country, :latitude, :longitude)
+    params.require(:location).permit(:result_string, :place_id)
   end
 end
