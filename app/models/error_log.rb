c4910bf3fc1c5950021301eb1fad7bba429628a5
Diana Eastman <diana.eastman@annkissam.com>
2017-06-26 23:03:27 -0400

    Change error log name.

--- app/models/error_log.rb
+++ app/models/error_log.rb
@@ -1,6 +1,6 @@
 class ErrorLog
   def self.debug(message=nil)
-    @error_log ||= Logger.new("#{Rails.root}/log/my.log")
+    @error_log ||= Logger.new("#{Rails.root}/log/error_log")
     @error_log.debug(message) unless message.nil?
   end
 end
